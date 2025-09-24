25d2684bc88f7b1e2543edd6234953c7daddbf9a
Peti <Vpeter993@gmail.com>
2025-09-24 15:40:07 +0200

    conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  // The output must be 'static' for GitHub Pages.
+  // This is the default, so we can remove the 'output' line.
+  
+  // Set the site and base for the GitHub Pages URL
+  site: 'https://H3AD5H0TPlay.github.io',
+  base: '/my_website',
+  
   integrations: [tailwind()],
-  site: 'https://portfolio.petervarga.dev', // Replace with your actual domain
-  output: 'server',
 });
